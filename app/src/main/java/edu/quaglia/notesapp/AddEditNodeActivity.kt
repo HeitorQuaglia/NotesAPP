6abb609c93ec12b3583f5f6a3702bfd0061da41d
Heitor Quaglia <heitor.bq@hotmail.com>
2022-01-20 20:19:56 -0300

    set up actions

---
+++ app/src/main/java/edu/quaglia/notesapp/AddEditNodeActivity.kt
@@ -0,0 +1,11 @@
+package edu.quaglia.notesapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AddEditNodeActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_edit_node)
+    }
+}
--- app/src/main/java/edu/quaglia/notesapp/MainActivity.kt
+++ app/src/main/java/edu/quaglia/notesapp/MainActivity.kt
@@ -1,13 +1,18 @@
 package edu.quaglia.notesapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.google.android.material.floatingactionbutton.FloatingActionButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NoteClickDeleteInterface, NoteClickInterface {
     lateinit var notesRV: RecyclerView
     lateinit var addFAB: FloatingActionButton
+    lateinit var viewModel: NoteViewModel
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -15,5 +20,45 @@ class MainActivity : AppCompatActivity() {
 
         notesRV = findViewById(R.id.idRVNotes)
         addFAB = findViewById(R.id.idFABAddNote)
+
+        notesRV.layoutManager = LinearLayoutManager(this)
+        val noteRVAdapter = NoteRVAdapter(
+            this,
+            this,
+            this
+        )
+        notesRV.adapter = noteRVAdapter
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        )[NoteViewModel::class.java]
+
+        viewModel.allNotes.observe(this, { list ->
+            list?.let {
+                noteRVAdapter.updateList(it)
+            }
+        })
+
+        addFAB.setOnClickListener{
+            val intent = Intent(this@MainActivity, AddEditNodeActivity::class.java)
+            startActivity(intent)
+            this.finish()
+        }
+    }
+
+    override fun onDeleteIconClick(note: Note) {
+        viewModel.deleteNote(note)
+        Toast.makeText(this, "${note.noteTitle} Deleted", Toast.LENGTH_LONG).show()
+    }
+
+    override fun onNoteClick(note: Note) {
+        val intent = Intent(this@MainActivity, AddEditNodeActivity::class.java)
+        intent.putExtra("noteType", "Edit")
+        intent.putExtra("noteTitle", note.noteTitle)
+        intent.putExtra("noteDescription", note.noteDescription)
+
+        intent.putExtra("noteID", note.id)
+        startActivity(intent)
+        this.finish()
     }
 }
