46fc40ea7d54feb617770160ab8975add678c9b7
Heitor Quaglia <heitor.bq@hotmail.com>
2022-01-20 20:52:32 -0300

    add edit note events

--- app/src/main/java/edu/quaglia/notesapp/AddEditNodeActivity.kt
+++ app/src/main/java/edu/quaglia/notesapp/AddEditNodeActivity.kt
@@ -1,11 +1,81 @@
 package edu.quaglia.notesapp
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import androidx.lifecycle.ViewModelProvider
+import java.text.SimpleDateFormat
+import java.util.*
 
 class AddEditNodeActivity : AppCompatActivity() {
+
+    lateinit var noteTitleEdt: EditText
+    lateinit var noteDescriptionEdt: EditText
+    lateinit var addUpdateBtn: Button
+    lateinit var viewModel: NoteViewModel
+    var noteID = -1
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_add_edit_node)
+
+        noteTitleEdt = findViewById(R.id.idEdtNoteTitle)
+        noteDescriptionEdt = findViewById(R.id.idEdtNoteDescription)
+        addUpdateBtn = findViewById(R.id.idBtnAddUpdate)
+
+        viewModel = ViewModelProvider(
+            this,
+            ViewModelProvider.AndroidViewModelFactory.getInstance(application)
+        )[NoteViewModel::class.java]
+
+        val noteType = intent.getStringExtra("noteType")
+
+        if (noteType.equals("Edit")) {
+            val noteTitle = intent.getStringExtra("noteTitle")
+            val noteDescription = intent.getStringExtra("noteDescription")
+            noteID = intent.getIntExtra("noteID", -1)
+
+            addUpdateBtn.text = "Update Note"
+            noteTitleEdt.setText(noteTitle)
+            noteDescriptionEdt.setText(noteDescription)
+        } else {
+            addUpdateBtn.text = "Save Note"
+        }
+
+        addUpdateBtn.setOnClickListener {
+            val noteTitle = noteTitleEdt.text.toString()
+            val noteDescription = noteDescriptionEdt.text.toString()
+
+            if (noteType.equals("Edit")) {
+                if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()) {
+                    val sdf = SimpleDateFormat("dd MMM, yyyy")
+                    val currentDate = sdf.format(Date())
+                    val updateNote = Note(noteTitle, noteDescription, currentDate)
+                    updateNote.id = noteID
+
+                    viewModel.updateNote(updateNote)
+                    Toast.makeText(this, "Note Updated", Toast.LENGTH_SHORT).show()
+                }
+            } else {
+                if (noteTitle.isNotEmpty() && noteDescription.isNotEmpty()) {
+                    val sdf = SimpleDateFormat("dd MMM, yyyy")
+                    val currentDate = sdf.format(Date())
+                    viewModel.addNote(
+                        Note(
+                            noteTitle,
+                            noteDescription,
+                            currentDate
+                        )
+                    )
+                    Toast.makeText(this, "Note Added...", Toast.LENGTH_SHORT).show()
+                }
+            }
+
+            startActivity(Intent(applicationContext, MainActivity::class.java))
+            this.finish()
+        }
     }
 }
